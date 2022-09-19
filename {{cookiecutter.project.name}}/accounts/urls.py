972e4713275cd7abb703dfc8702c59845a5682b9
saksham-ti <110182987+saksham-ti@users.noreply.github.com>
2022-09-19 10:46:53 +0400

    added Pytest requirements

--- {{cookiecutter.project.name}}/accounts/urls.py
+++ {{cookiecutter.project.name}}/accounts/urls.py
@@ -4,4 +4,4 @@ from .views import UserViewSet
 
 # Routers provide an easy way of automatically determining the URL conf.
 router = DefaultRouter()
-router.register(r'users', UserViewSet)
+router.register(r'users', UserViewSet, basename="user-detail")
