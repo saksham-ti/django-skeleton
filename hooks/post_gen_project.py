cfab1b7e37fd0460340f6f9f4c962c2da1677108
saksham-ti <110182987+saksham-ti@users.noreply.github.com>
2022-09-23 17:16:03 +0400

    Update post_gen_project.py

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -2,6 +2,7 @@ import os
 import shutil
 
 
+
 def append_to_gitignore_file(ignored_line):
     with open(".gitignore", "a") as gitignore_file:
         gitignore_file.write(ignored_line)
@@ -14,7 +15,7 @@ REMOVE_PATHS = [
     {% endif %}
 
     {%- if not cookiecutter.tests.enabled == 'True' %}
-    'conftest',
+    'conftest.py',
     'tests',
     {%- endif %}
 ]
@@ -26,7 +27,7 @@ def main():
         path = path.strip()
         if path and os.path.exists(path):
             if os.path.isdir(path):
-                os.rmdir(path)
+                shutil.rmtree(path)
             else:
                 os.unlink(path)
 
